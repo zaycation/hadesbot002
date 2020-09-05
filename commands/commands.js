104ceac1fe9c694310d632f2a2fb6a282707ad04
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-04 18:20:00 -0700

    slight edits to embeds with MD2

--- commands/commands.js
+++ commands/commands.js
@@ -6,7 +6,9 @@ module.exports = {
 	execute(message) {
         message.channel.send({embed: {
             color: 15844367,
-            description: 'Here\'s a list of all my commands:\n\n`[Always remember, my prefix is !]`\n\n!args-info\n!avatar\n!help\n!kick\n!ping\n!purge\n!server-info\n!user-info\n\nAlways remember you can run `!help <command>` for additional info on how each command works'
+            title: 'Here\'s a list of all my commands:',
+            description: '\n`[Always remember, my prefix is !]`\n\n!args-info\n!avatar\n!help\n!kick\n!ping\n!purge\n!server-info\n!user-info\n\n',
+            footer: 'Always remember you can run `!help <command>` for additional info on how each command works'
           }});
     },
 };

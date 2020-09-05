e50b3091efff056da8bd544aecc0f4f6c23812dd
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-04 17:45:27 -0700

    tokenplay2

--- commands/user-info.js
+++ commands/user-info.js
@@ -4,7 +4,8 @@ module.exports = {
 	execute(message) {
         message.channel.send({embed: {
             color: 15844367,
-            description: `Discord Tag: ${message.author.tag}\nYour ID: ${message.author.id}\nCreated: ${message.author.createdAt}\n\nIs User A Bot? ${message.author.bot}\n\nLast Message: \n> ${message.author.lastMessage}\n\nUser's Avatar: ${message.author.displayAvatarURL({ dynamic: true })}`
+            title: `Discord Tag: ${message.author.tag}`,
+            description: `\nYour ID: ${message.author.id}\nCreated: ${message.author.createdAt}\n\nIs User A Bot? ${message.author.bot}\n\nLast Message: \n> ${message.author.lastMessage}\n\nUser's Avatar: ${message.author.displayAvatarURL({ dynamic: true })}`
           }});
     },
 };

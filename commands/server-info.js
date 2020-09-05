03cf126c8fadfb3129a59d8cb9fe3f9a30bb9a2e
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-04 17:38:12 -0700

    debug serverinfo 2

--- commands/server-info.js
+++ commands/server-info.js
@@ -6,7 +6,6 @@ module.exports = {
 			color: 15844367,
 			title: `${message.guild.name}`,
 			description: `\nRegion: ${message.guild.region}\nTotal members: ${message.guild.memberCount}\nNumber of Boosts: ${message.guild.premiumSubscriptionCount} | Boost Tier: ${message.guild.premiumTier}\nServer Owner: ${message.guild.owner} | ID: ${message.guild.ownerID}`,
-			timestamp: new Date(),
           }});
     },
 };

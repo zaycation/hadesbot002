6f7c3bcd160ae551a8ce183855d4ab3541a1190d
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-05 18:34:31 -0700

    kick cmd edit3

--- commands/kick.js
+++ commands/kick.js
@@ -5,18 +5,18 @@ module.exports = {
 		
 		if(!message.member.hasPermission('KICK_MEMBERS'))
         message.channel.send("You don't have permission to use that command.");
-    else {
-        let member = message.guild.members.cache.get(args);
-        if(member) {
-        try {
-            await member.kick();
-            console.log('Kicked');
-            message.channel.send(`${member}, Kicked!`)
-        }
-            catch(err) {
-            console.log(err);
-        }
-    }
-}
+		else {
+			let member = message.guild.members.cache.get(args);
+		if(member) {
+			try {
+				await member.kick();
+				console.log('Kicked');
+				message.channel.send(`${member}, Kicked!`)
+			}
+				catch(err) {
+				console.log(err);
+				}
+			}
+		}
 	},
 };

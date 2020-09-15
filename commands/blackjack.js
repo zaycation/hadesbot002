82aa82316f60b7b76ad850b6f5de3ba62f455076
zaycation <contact@zaycation.dev>
2020-09-15 13:04:35 -0700

    Update blackjack.js

--- commands/blackjack.js
+++ commands/blackjack.js
@@ -1,4 +1,20 @@
-const Discord = require("discord.js);
 module.exports = {
-
-}
+  name: 'blackjack',
+  aliases: ['bj'],
+  description: 'simple blackjack game',
+  execute(message) {
+    if(!message.guild.me.hasPermission('ADD_REACTIONS'))
+      return message.reply ('I do not have the proper perms to add reactions!!');
+    let playerCard1 = Math.floor(Math.random() * 11) + 2;
+    let playerCard2 = Math.floor(Math.random() * 11) + 2;
+    let botCard1 = Math.floor(Math.random() * 11) + 2;
+    let botCard2 = Math.floor(Math.random() * 11) + 2;
+    let playerTotal = playerCard1 + playerCard2;
+    let playerArray =[];
+    let botArray = [];
+    let botTotal = botCard1 + botCard2;
+    let embed = new Discord.MessageEmbed()
+    .setTitle('Blackjack')
+    .setDescription(`Your hand: ${playerCard1},${playerCard2}:${playerTotal}\nHades hand: ${botCard1},${botCard2}:${botTotal}\n`)
+  },
+};

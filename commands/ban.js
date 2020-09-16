ea771cf79aeb34b13dffc11e63058cc71f240bcd
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-16 12:49:44 -0700

    ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -9,26 +9,26 @@ module.exports = {
 
     if (!target) {
       return message.channel.send(
-        `**${message.author.username}**, Please mention the person who you want to kick`
+        `**${message.author.username}**, Please @ mention the person you want to ban`
       );
     } else if (target.id === message.author.id) {
       return message.channel.send(
-        `**${message.author.username}**, You can not kick yourself`
+        `**${message.author.username}**, You can not ban yourself idiot smh`
       );
     } else if (!args[1]) {
       return message.channel.send(
-        `**${message.author.username}**, Please Give Reason to kick`
+        `**${message.author.username}**, Please provide me with a reason to ban dumbass...`
       );
     }
 
     message.channel.send({
       embed: {
         color: 15844367,
-        title: "Action: Kick",
-        description: `Kicked ${target} | ID: (${target.id})\nReason: ${reason}\nMod: ${message.author.username}`,
+        title: "Action: Ban",
+        description: `Banned ${target} | ID: (${target.id})\nReason: ${reason}\nMod: ${message.author.username}`,
       },
     });
 
-    target.kick(args[1]);
+    target.ban(args[1]);
   },
 };
--- commands/kick.js
+++ commands/kick.js
@@ -1,34 +1,35 @@
 module.exports = {
-  name: "kick",
-  category: "moderation",
-  description: "Kick anyone with one shot xD",
-  usage: "kick <@user> <reason>",
-  execute(message, args) {
-
-    let target = message.mentions.members.first();
-
-    if (!target) {
-      return message.channel.send(
-        `**${message.author.username}**, Please mention the person who you want to kick`
-      );
-    } else if (target.id === message.author.id) {
-      return message.channel.send(
-        `**${message.author.username}**, You can not kick yourself`
-      );
-    } else if (!args[1]) {
-      return message.channel.send(
-        `**${message.author.username}**, Please Give Reason to kick`
-      );
-    }
-
-    message.channel.send({
-      embed: {
-        color: 15844367,
-        title: "Action: Kick",
-        description: `Kicked ${target} (${target.id})\nKicked by ${message.author.username}`,
-      },
-    });
-
-    target.kick(args[1]);
-  },
-};
+	name: "kick",
+	category: "moderation",
+	description: "Kick anyone with one shot xD",
+	usage: "kick <@user> <reason>",
+	execute(message, args) {
+  
+	  let target = message.mentions.members.first();
+  
+	  if (!target) {
+		return message.channel.send(
+		  `**${message.author.username}**, Please @ mention the person you want to kick`
+		);
+	  } else if (target.id === message.author.id) {
+		return message.channel.send(
+		  `**${message.author.username}**, You can not kick yourself idiot smh`
+		);
+	  } else if (!args[1]) {
+		return message.channel.send(
+		  `**${message.author.username}**, Please provide me with a reason to kick dumbass...`
+		);
+	  }
+  
+	  message.channel.send({
+		embed: {
+		  color: 15844367,
+		  title: "Action: Kick",
+		  description: `Kicked ${target} | ID: (${target.id})\nReason: ${reason}\nMod: ${message.author.username}`,
+		},
+	  });
+  
+	  target.kick(args[1]);
+	},
+  };
+  
